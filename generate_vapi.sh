5fb356aa65f65138f860785ae4f2ba2077d15404
piotras <piotrek.pokora@gmail.com>
2011-10-10 21:27:03 +0200

    Generate Midgard.vapiinstead of midgard2 one

--- generate_vapi.sh
+++ generate_vapi.sh
@@ -1,3 +1,3 @@
 vala-gen-introspect midgard2 ./
-vapigen --library midgard2 midgard2.gi
-perl -p -i -e  "s/midgard2.h/midgard\/midgard.h/g" midgard2.vapi
+vapigen --library Midgard midgard2.gi
+perl -p -i -e  "s/midgard2.h/midgard\/midgard.h/g" Midgard.vapi
